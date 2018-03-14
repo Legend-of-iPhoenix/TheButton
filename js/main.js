7360560b897f306acc636f296abbb09c1a8a9078
Travis Evans <travisgevans@gmail.com>
2018-03-14 16:37:33 -0500

    Number entries on the leaderboard

--- js/main.js
+++ js/main.js
@@ -121,7 +121,8 @@ function ready() {
     }
   }, 50);
   // </copyright>
-  firebase.database().ref("/button/users/").orderByValue().limitToLast(5).on('value',function(snapshot) {
+  var leaderboardLength = 5;
+  firebase.database().ref("/button/users/").orderByValue().limitToLast(leaderboardLength).on('value',function(snapshot) {
     var scores = document.getElementById("highscores");
     scores.innerHTML = "";
     var x = n => {
@@ -138,12 +139,14 @@ function ready() {
       }
       return "N/A"
     }
+    var rank = leaderboardLength;
     snapshot.forEach(function(childSnapshot) {
-      scores.innerHTML+="<tr><td>"+cleanse(childSnapshot.key)+"</td><td>"+x(childSnapshot.val())+"</td></tr>"
+      scores.innerHTML+="<tr><td>"+rank+"</td><td>"+cleanse(childSnapshot.key)+"</td><td>"+x(childSnapshot.val())+"</td></tr>";
+      rank--;
     });
     //reverse ordering of elements
     (e=>{for(var d=0;d<e.childNodes.length;d++)e.insertBefore(e.childNodes[d],e.firstChild)})(document.getElementById("highscores"));
-    scores.innerHTML = "<tr><th>Username</th><th>Time</th></tr>" + scores.innerHTML;
+    scores.innerHTML = "<tr><th>&nbsp;</th><th>Username</th><th>Time</th></tr>" + scores.innerHTML;
   });
   document.getElementById("TheButton").click=x=>console.log("Abuse is not tolerated.");
   document.getElementById("TheButton").onfocus=x=>document.getElementById("TheButton").blur();
