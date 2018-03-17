3530f2422ad9312631ecec992a7babb6a624a354
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-03-17 17:16:12 -0400

    Added tev's updates

--- js/main.js
+++ js/main.js
@@ -148,12 +148,12 @@ function ready() {
     }
     var rank = leaderboardLength;
     snapshot.forEach(function(childSnapshot) {
-      scores.innerHTML+="<tr><td>"+rank+"</td><td>"+cleanse(childSnapshot.key)+"</td><td>"+x(childSnapshot.val())+"</td></tr>";
+      scores.innerHTML+="<tr><td class=\"lbrank\">"+rank+"</td><td class=\"lbusername\">"+cleanse(childSnapshot.key)+"</td><td class\"lbtime\">"+x(childSnapshot.val())+"</td></tr>";
       rank--;
     });
     //reverse ordering of elements
     (e=>{for(var d=0;d<e.childNodes.length;d++)e.insertBefore(e.childNodes[d],e.firstChild)})(document.getElementById("highscores"));
-    scores.innerHTML = "<tr><th>&nbsp; Rank &nbsp; &nbsp;</th><th>&nbsp; &nbsp; Username</th><th>&nbsp; &nbsp; &nbsp; &nbsp; Time</th></tr>" + scores.innerHTML;
+    scores.innerHTML = "<tr><th class=\"lbrank\">&nbsp;</th><th class=\"lbusername\">Username</th><th class=\"lbtime\">Time</th></tr>" + scores.innerHTML;
   });
   document.getElementById("TheButton").click=x=>console.log("Abuse is not tolerated.");
   document.getElementById("TheButton").onfocus=x=>document.getElementById("TheButton").blur();
