03c79c69cf759dcbc71d39d6ae8f0d6bed5f8c41
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-03-22 21:15:54 -0400

    Revert "Night Mode Experiment"

--- js/main.js
+++ js/main.js
@@ -26,11 +26,7 @@ function go() {
   document.getElementById('logout-span').classList = 'visible';
   document.getElementById('login-div').classList = 'hidden';
 }
-  
-  document.querySelector('[data-switch-contrast]').addEventListener('click', function() {
-  document.body.classList.toggle('nightmode');
-});
-  
+
 function logout() {
   document.getElementById('main-div').classList = 'hidden';
   document.getElementById('logout-span').classList = 'hidden';
