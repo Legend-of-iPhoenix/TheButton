4a1061073ac14867b523bc362bee5f4c74fff166
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-03-28 17:30:11 -0400

    Prevent site from being loaded in iFrame.

--- js/main.js
+++ js/main.js
@@ -225,6 +225,9 @@ var linkOverride;
 */
 var otherRepos = [];
 window.onload = e => {
+  if (window.top !== window.self) {
+    document.body.innerHTML = "<p><strong>Please do not load TheButton in an iFrame element. If you think this is an error, please contact _iPhoenix_</strong></p><br /><p>Thank you.</p>"
+  }
   var i = location.hostname.split("").reverse().join("").substring(10).split("").reverse().join(""),
   this_repo_url = (linkOverride ? linkOverride : 'https://github.com/' + i + '/' + location.pathname.split('/')[1]);
   is_original = i == 'legend-of-iphoenix';
