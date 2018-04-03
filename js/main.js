e6b69c05cf53d48c0d7f77ae609c9261e1169b4b
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-04-03 16:41:58 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,11 +34,25 @@ var lightButtonEnabled = false;
     document.getElementById('login-div').classList = 'visible';
   }
   
-  function togglenightmode() {
-    var element = document.getElementById("body");
-    element.classList.toggle("nightmode");
+  (function() {
+  function toggleNightMode() {
+    var elem = document.getElementById(this.dataset.target);
+    
+    if (this.dataset.mode === 'off') {
+      elem.style.color = '#000';
+      elem.style.backgroundColor = '#fff';
+      elem.style.fontFamily = 'Ubuntu, sans-serif';
+      this.dataset.mode = 'on';
+    } else {
+      elem.style.color = '#fff';
+      elem.style.backgroundColor = '#000';
+      elem.style.fontFamily = '"Ubuntu", sans-serif';
+      this.dataset.mode = 'off';
+    }
   }
-
+  
+  document.getElementById('togBut').addEventListener('click', toggleNightMode);
+})();
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       if (user.photoURL == "https://legend-of-iphoenix.github.io/TheButton/img/authenticated.png" && /^\w{1,32}$/.test(user.displayName) && user.displayName) {
